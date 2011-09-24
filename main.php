a4079ca330921dda7fdbcc3c9355d87e9ea4173f
Lauri <lauri.sakari.makinen@gmail.com>
2011-09-24 13:03:00 +0300

    Added commit count and ins/del statistics

--- main.php
+++ main.php
@@ -5,6 +5,10 @@
 //
 //version: 1.0
 
+//TODO:
+// * Way to change the threshold
+// * Display commit count and average lines per commit
+
 class CodeAnalysisPlugin extends VGPlugin{
 	function __construct(){
 		global $conf;
@@ -23,9 +27,14 @@ class CodeAnalysisPlugin extends VGPlugin{
 
             $output = run_git($project, "log --shortstat --reverse --pretty=format%ct");
             
-            $current_lines = 0;
-            $current_times = 0;
+            $current_lines      = 0;
+            $current_times      = 0;
             $true_current_lines = 0;
+            $commit_count       = 0;
+            $total_ins          = 0;
+            $total_del          = 0;
+            $total_ins_real     = 0;
+            $total_del_real     = 0;
 
             $graph_data[] = array();
             $graph_time[] = array();
@@ -43,18 +52,25 @@ class CodeAnalysisPlugin extends VGPlugin{
                     $del    = $blob[6];
                     $result = $ins - $del;
 
-                    //Add to the line count
+                    $commit_count++;
 
                     //check for added libraries/log files anything else big that would mess up the whole damn thing
                     if($result < 400 && $result > -400){
-                        $current_lines = $current_lines + $result;
                         array_push($graph_data,$current_lines);
                         array_push($graph_time,$unixtime);
 
+                        $current_lines = $current_lines + $result;
+                        $total_ins = $total_ins + $ins;
+                        $total_del = $total_del + $del;
+
                         $true_current_lines = $true_current_lines + $result;
+                        $true_total_ins = $true_total_ins + $ins;
+                        $true_total_del = $true_total_del + $del;
                     }else{
                         //Change the true count even if there is HUGE commit blob
                         $true_current_lines = $true_current_lines + $result;
+                        $true_total_ins = $true_total_ins + $ins;
+                        $true_total_del = $true_total_del + $del;
                     }
 
                     //echo("$current_lines ($result) <br/>");
@@ -69,9 +85,21 @@ class CodeAnalysisPlugin extends VGPlugin{
             }
 
             echo("<p>Total lines: $current_lines</p>");
+
+            $loc_per_commit = $current_lines / $commit_count;
+            $loc_per_commit = round($loc_per_commit,2);
+            echo("<p>Commits: $commit_count ($loc_per_commit average loc per commit)</p>");
+
+            echo("<p>Insertions: $total_ins</p>");
+            echo("<p>Deletions: $total_del</p>");
+
             if($current_lines <> $true_current_lines){
+                echo("<hr>");
                 echo("<p>True line count: $true_current_lines (may contain log files and external libraries)</p>");
+                echo("<p>True insertion count: $true_total_ins</p>");
+                echo("<p>True deletion count: $true_total_del</p>");
             }
+
             echo("<h3>Graph</h3>");
 
             echo("<script id='source' language='javascript' type='text/javascript'>");
